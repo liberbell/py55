9143c9298547405eb9800845f120b851b7329f2b
eliberbell <liberbell@gmail.com>
2026-08-07 08:59:08 +0900

    shapely2

--- lesson1/shapely.py
+++ lesson1/shapely.py
@@ -2,3 +2,8 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib as plt
 from shapely.geometry import Point, LineString, Polygon
+
+point = Point(0.0, 1.0)
+
+output_file = "shapely_result.html"
+m.save(output_file)
