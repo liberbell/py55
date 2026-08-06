544865cd376abb2d71df5300a23fe50e877e031e
eliberbell <liberbell@gmail.com>
2026-08-07 08:38:44 +0900

    demo11

--- lesson1/demo1.py
+++ lesson1/demo1.py
@@ -15,4 +15,5 @@ gdf = gdf.to_crs(epsg=4326)
 print(gdf.crs)
 print(gdf.head())
 
-m = folium.Map(location=[40.7, -73.94], zoom_start=10)
+m = folium.Map(location=[40.7, -73.94], zoom_start=10, tiles="CartDB positron")
+print(m)
