d80346e94ea194bbdbda48bd66990d53c8f71271
eliberbell <liberbell@gmail.com>
2026-08-07 08:48:31 +0900

    demo14

--- lesson1/demo1.py
+++ lesson1/demo1.py
@@ -16,5 +16,11 @@ print(gdf.crs)
 print(gdf.head())
 
 m = folium.Map(location=[40.7, -73.94], zoom_start=10, tiles="CartoDB positron")
+
+for _, r in gdf.itterrows():
+    sim_geo = gpd.GeoSeries(r["geometry"]).simplify(tolerance=0.001)
+    geo_j = sim_geo.to_json()
+    geo_j = folium.GeoJson(data=geo_j, style_function=lambda x: {"fillcolor": "orange"})
+
 output_file = "map.html"
 m.save(output_file)
