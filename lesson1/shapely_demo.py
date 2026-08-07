df312155b747453152aa70803c947d4c8ea9ced3
eliberbell <liberbell@gmail.com>
2026-08-07 09:00:55 +0900

    shapely_demo1

--- lesson1/shapely_demo.py
+++ lesson1/shapely_demo.py
@@ -4,6 +4,7 @@ import matplotlib as plt
 from shapely.geometry import Point, LineString, Polygon
 
 point = Point(0.0, 1.0)
+print(point)
 
-output_file = "shapely_result.html"
-m.save(output_file)
+# output_file = "shapely_result.html"
+# m.save(output_file)
