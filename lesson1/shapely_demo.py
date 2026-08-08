ff2fd87d56e0dce489a59158b7ec72d98c8a7fff
eliberbell <liberbell@gmail.com>
2026-08-08 20:01:43 +0900

    shapely_demo4

--- lesson1/shapely_demo.py
+++ lesson1/shapely_demo.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import geopandas as gpd
 import matplotlib as plt
-from shapely.geometry import Point, LineString, Polygon
+from shapely.geometry import Point, LineString, Polygon, MultiPoint, MultiLineString, MultiPolygon
 
 point = Point(0.0, 1.0)
 print(point)
