4c1ecd048fefbb20407f4aeb3b5cda43f35dd809
eliberbell <liberbell@gmail.com>
2026-08-08 19:51:17 +0900

    shapely_demo1

--- lesson1/shapely_demo.py
+++ lesson1/shapely_demo.py
@@ -6,5 +6,10 @@ from shapely.geometry import Point, LineString, Polygon
 point = Point(0.0, 1.0)
 print(point)
 
-# output_file = "shapely_result.html"
-# m.save(output_file)
+line = LineString([(0, 0), (1, 1), (3, 0), (5, 2)])
+
+gdf = gpd.GeoDataFrame(geometry=[line], crs="EPSG:4326")
+
+m = gdf.explore()
+output_file = "shapely_result.html"
+m.save(output_file)
