99a717b51cfc82e4247871c1aa92687f31bb1583
eliberbell <liberbell@gmail.com>
2026-08-10 08:45:41 +0900

    geo_exdata2

--- lesson3/geo_exdata.py
+++ lesson3/geo_exdata.py
@@ -5,4 +5,11 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Point, LineString, Polygon
 import geodatasets
 
-print(geodatasets.data)
+# print(geodatasets.data)
+
+data_path = geodatasets.get_path("naturalearth.lowres")
+
+gdf1 = gpd.read_file(data_path)
+
+
+print(gdf1)
