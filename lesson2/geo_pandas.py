33c1a4fe8122acf9e12fb791e0ff935fe2fe447c
eliberbell <liberbell@gmail.com>
2026-08-10 08:28:26 +0900

    geo_pandas1

--- lesson2/geo_pandas.py
+++ lesson2/geo_pandas.py
@@ -5,3 +5,11 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Point, LineString, Polygon
 
 d = {"cal1": ["A", "B"], "geometry": [Point(0, 1), Point(1, 0)]}
+gdf = gpd.GeoDataFrame(d)
+
+# gdf = gpd.GeoDataFrame(geometry=[result], crs="EPSG:4326")
+
+
+m = gdf.explore()
+output_file = "gpd_result.html"
+m.save(output_file)
