27ca9e63ea0ea12e0ad3b35122de7be58ea0c1e5
eliberbell <liberbell@gmail.com>
2026-08-10 08:42:39 +0900

    geo_exdata1

---
+++ lesson3/geo_exdata.py
@@ -0,0 +1,8 @@
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from shapely.geometry import Point, LineString, Polygon
+import geodatasets
+
+print(geodatasets.data)
