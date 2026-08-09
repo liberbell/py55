5c777b9d60c2ef387a64ae933f01a5eae5494cfd
eliberbell <liberbell@gmail.com>
2026-08-10 08:35:24 +0900

    geo_pandas5

--- lesson2/geo_pandas.py
+++ lesson2/geo_pandas.py
@@ -7,7 +7,7 @@ from shapely.geometry import Point, LineString, Polygon
 # d = {"cal1": ["A", "B"], "geometry": [Point(0, 1), Point(1, 0)]}
 # gdf = gpd.GeoDataFrame(d)
 
-d = {"cal1": ["A", "B"], "geometry": [LineString([(0, 1), (1, 0)]), LineString(1, 0)]}
+d = {"cal1": ["A", "B"], "geometry": [LineString([(0, 1), (1, 0)]), LineString([(2, 0), (2, 2)])]}
 gdf = gpd.GeoDataFrame(d)
 
 print(gdf, type(gpd))
