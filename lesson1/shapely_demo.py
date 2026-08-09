429f4e8947c0496e8c0587561d724158363415fe
eliberbell <liberbell@gmail.com>
2026-08-09 11:48:07 +0900

    shapely_demo1

--- lesson1/shapely_demo.py
+++ lesson1/shapely_demo.py
@@ -21,7 +21,7 @@ a = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
 b = Polygon([(2, 0), (2, 1), (3, 1), (3, 0)])
 polygons = MultiPolygon([a, b])
 
-gdf = gpd.GeoDataFrame(geometry=[lines], crs="EPSG:4326")
+gdf = gpd.GeoDataFrame(geometry=[polygons], crs="EPSG:4326")
 
 
 m = gdf.explore()
