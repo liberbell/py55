abfbf5eda3eef50b6d804df60ab531c59539b24d
eliberbell <liberbell@gmail.com>
2026-08-09 15:19:47 +0900

    geo_pandas2

--- lesson2/geo_pandas.py
+++ lesson2/geo_pandas.py
@@ -3,3 +3,5 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, LineString, Polygon
+
+d = {"cal1": ["A", "B"], "geometry": [Point(0, 1), Point(1, 0)]}
