5dbd5b3695a994ba8b77f3e40e0cb9f8988e8e74
eliberbell <liberbell@gmail.com>
2026-08-10 09:00:09 +0900

    geo_exdata5

--- lesson3/geo_exdata.py
+++ lesson3/geo_exdata.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, LineString, Polygon
+import geodatasets
 from geodatasets import get_path
 
 # print(geodatasets.data)
 
 # data_path = geodatasets.get_path("naturalearth.lowres")
 # data_path = "https://githubusercontent.com"
-data_path = get_path('naturalearth.lowres')
+data_path = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
 
 gdf1 = gpd.read_file(data_path)
 
