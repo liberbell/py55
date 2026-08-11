4f4cc4854cf38c6b8b3803111bbe3a50249636d3
eliberbell <liberbell@gmail.com>
2026-08-12 08:35:03 +0900

    exdata5

--- lesson4/exdata.py
+++ lesson4/exdata.py
@@ -6,8 +6,9 @@ from shapely.geometry import Point, LineString, Polygon
 import geodatasets
 from geodatasets import get_path
 
-gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.shp", encoding="shift-jis")
-print(gdf)
+# gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.shp", encoding="shift-jis")
+gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.geojson", encoding="utf-8")
+# print(gdf)
 
 m = gdf.explore()
 output_file = "train_exdata.html"
