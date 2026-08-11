923bd052641f79c2aed3a5f9ab081faf4ef777b5
eliberbell <liberbell@gmail.com>
2026-08-12 08:48:50 +0900

    exdata12

--- lesson4/exdata.py
+++ lesson4/exdata.py
@@ -9,9 +9,12 @@ from geodatasets import get_path
 # gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.shp", encoding="shift-jis")
 gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.geojson", encoding="utf-8")
 # print(gdf["N02_003"].unique())
-print(gdf)
-print(gdf["路線名"].unique())
+# print(gdf)
+# print(gdf["路線名"].unique())
+
+gdf_yamanote = gdf[gdf["路線名"]== "山手線"]
 
 # m = gdf.explore(width=500, height=800)
-# output_file = "train_exdata.html"
-# m.save(output_file)
+m = gdf_yamanote.explore()
+output_file = "train_exdata.html"
+m.save(output_file)
