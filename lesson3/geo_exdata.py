44a2bb983129e7b4182ecb1c7dd932eb637ac3b2
eliberbell <liberbell@gmail.com>
2026-08-11 12:28:22 +0900

    geo_exdata4

--- lesson3/geo_exdata.py
+++ lesson3/geo_exdata.py
@@ -12,11 +12,12 @@ from geodatasets import get_path
 # df = pd.DataFrame.from_dict(geodatasets.data, orient='index')
 # print(df)
 
-data_path = geodatasets.get_path("airbnb")
+data_path = geodatasets.get_path("geoda.airbnb")
 # data_path = "https://githubusercontent.com"
 # data_path = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
 
-gdf1 = gpd.read_file(data_path)
+gdf = gpd.read_file(data_path)
 
-
-print(gdf1)
+m = gdf.explore()
+output_file = "exdata_result.html"
+m.save(output_file)
