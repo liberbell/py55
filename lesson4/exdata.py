938065ff1aa7693308f318a504aff4735ba759be
eliberbell <liberbell@gmail.com>
2026-08-12 08:46:01 +0900

    exdata11

--- lesson4/exdata.py
+++ lesson4/exdata.py
@@ -10,7 +10,7 @@ from geodatasets import get_path
 gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.geojson", encoding="utf-8")
 # print(gdf["N02_003"].unique())
 print(gdf)
-print(gdf["N02_003"].unique())
+print(gdf["路線名"].unique())
 
 # m = gdf.explore(width=500, height=800)
 # output_file = "train_exdata.html"
