b36bcf5d005ad8b37371a4c89cc54485e33db19e
eliberbell <liberbell@gmail.com>
2026-08-12 08:37:16 +0900

    exdata6

--- lesson4/exdata.py
+++ lesson4/exdata.py
@@ -10,6 +10,9 @@ from geodatasets import get_path
 gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.geojson", encoding="utf-8")
 # print(gdf)
 
-m = gdf.explore()
+fig = plt.figure(figsize=(8, 10))
+ax = fig.add_subplot(1, 1, 1)
+
+m = gdf.explore(m=ax)
 output_file = "train_exdata.html"
 m.save(output_file)
