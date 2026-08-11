af1420a27d9670c870f797be3831ae44fda46145
eliberbell <liberbell@gmail.com>
2026-08-11 12:26:32 +0900

    geo_exdata3

--- lesson3/geo_exdata.py
+++ lesson3/geo_exdata.py
@@ -16,7 +16,7 @@ data_path = geodatasets.get_path("airbnb")
 # data_path = "https://githubusercontent.com"
 # data_path = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
 
-# gdf1 = gpd.read_file(data_path)
+gdf1 = gpd.read_file(data_path)
 
 
-# print(gdf1)
+print(gdf1)
