d983bd0807d44d6209ed89cf5de1015f3beb1f33
eliberbell <liberbell@gmail.com>
2026-08-13 08:49:03 +0900

    exdata3

--- lesson4/exdata.py
+++ lesson4/exdata.py
@@ -6,6 +6,8 @@ from shapely.geometry import Point, LineString, Polygon
 import geodatasets
 from geodatasets import get_path
 
+pd.set_option('display.max_columns', 1000)
+
 # gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.shp", encoding="shift-jis")
 # gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.geojson", encoding="utf-8")
 # print(gdf["N02_003"].unique())
