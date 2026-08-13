b0807bba7ebcb86821b88f50a160f60545b56c98
eliberbell <liberbell@gmail.com>
2026-08-14 08:40:05 +0900

    exdata4

--- lesson4/exdata.py
+++ lesson4/exdata.py
@@ -6,7 +6,8 @@ from shapely.geometry import Point, LineString, Polygon
 import geodatasets
 from geodatasets import get_path
 
-pd.set_option('display.max_columns', 1000)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
 # gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.shp", encoding="shift-jis")
 # gdf = gpd.read_file("N02-19_GML/N02-19_RailroadSection.geojson", encoding="utf-8")
@@ -25,7 +26,7 @@ geometry = gpd.points_from_xy(df["経度"], df["緯度"])
 gdf = gpd.GeoDataFrame(df, geometry=geometry)
 
 gyosei = gpd.read_file("N03-20240101_13_GML/N03-20240101_13.shp")
-# print(gyosei)
+print(gyosei)
 
 gyousei_temp = gyosei.dropna(subset=["N03_003"])
 # print(gyousei_temp)
