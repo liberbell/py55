8ce0fb710d8f7f6932aad0f73dff5882b6e462ce
eliberbell <liberbell@gmail.com>
2026-08-23 21:32:14 +0800

    osm_map4

--- lesson5/osm_map.py
+++ lesson5/osm_map.py
@@ -6,4 +6,21 @@ from shapely.geometry import Point, LineString, Polygon
 query = "Yokohama-shi, Kanagawa, Japan"
 G = ox.graph_from_place(query, network_type="drive")
 
-G.save("kanagawa_road_map.html")
+# m = ox.plot_graph_folium(G, tiles="OpenStreetMap", color="blue", weight=2)
+
+gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
+center_lat = gdf_nodes["y"].mean()
+center_lng = gdf_nodes["x"].mean()
+
+m = folium.Map(location=[center_lat, center_lng], zoom_start=12, tiles="OpenStreetMap")
+
+folium.GeoJson(
+    gdf_edges,
+    style_function=lambda feature: {
+        "color": "blue",
+        "weight": 2,
+        "opacity": 0.7
+    }
+).add_to(m)
+
+m.save("kanagawa_road_map.html")
