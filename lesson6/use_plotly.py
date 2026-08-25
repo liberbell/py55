888b43d7aaa6876aff744a2c31bd70c33fa0a174
eliberbell <liberbell@gmail.com>
2026-08-25 21:38:22 +0900

    use_plotly3

--- lesson6/use_plotly.py
+++ lesson6/use_plotly.py
@@ -3,5 +3,5 @@ import plotly.express as px
 df = px.data.gapminder()
 # print(df)
 
-figure = px.scatter(df, x="gdpPercap", y="lifeExp")
+figure = px.scatter(df, x="gdpPercap", y="lifeExp", log_x=True)
 figure.show()
