332e838c482a2e9813759eef7b573176d89d4f0e
eliberbell <liberbell@gmail.com>
2026-08-26 08:45:35 +0900

    use_plotly1

--- lesson6/use_plotly.py
+++ lesson6/use_plotly.py
@@ -3,5 +3,8 @@ import plotly.express as px
 df = px.data.gapminder()
 # print(df)
 
-figure = px.scatter(df, x="gdpPercap", y="lifeExp", log_x=True)
-figure.show()
+# figure = px.scatter(df, x="gdpPercap", y="lifeExp", log_x=True, hover_name="continent")
+
+figure = px.choropleth(df[df["year"]==2007], locations="iso_alpha", color="gdpPercap")
+
+# figure.show()
