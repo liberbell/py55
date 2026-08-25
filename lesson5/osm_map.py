914f5b66278f3892ff5a20241c882aef607d92a9
eliberbell <liberbell@gmail.com>
2026-08-25 21:08:52 +0900

    osm_map6

--- lesson5/osm_map.py
+++ lesson5/osm_map.py
@@ -16,20 +16,20 @@ center_lng = gdf_nodes["x"].mean()
 
 m = folium.Map(location=[center_lat, center_lng], zoom_start=12, tiles="OpenStreetMap")
 
-folium.GeoJson(
-    gdf_edges,
-    style_function=lambda feature: {
-        "color": "blue",
-        "weight": 2,
-        "opacity": 0.7
-    }
-).add_to(m)
+# folium.GeoJson(
+#     gdf_edges,
+#     style_function=lambda feature: {
+#         "color": "blue",
+#         "weight": 2,
+#         "opacity": 0.7
+#     }
+# ).add_to(m)
 
 gdf1 = ox.graph_to_gdfs(G)[0]
-print(gdf1)
+# print(gdf1)
 
 gdf2 = ox.graph_to_gdfs(G)[1]
-print(gdf2)
+# print(gdf2)
 
 skytree = (35.7100, 139.8108)
 skytree_x = 139.8093
@@ -47,7 +47,7 @@ print(f"終点ノードID: {end}")
 shortest_path = ox.shortest_path(G, start, end)
 
 route_gdf = ox.routing.route_to_gdf(G, shortest_path)
-m = folium.Map(location=[skytree_y, skytree_x], zoom_start=14)
+# m = folium.Map(location=[skytree_y, skytree_x], zoom_start=14)
 
 folium.GeoJson(
     route_gdf,
