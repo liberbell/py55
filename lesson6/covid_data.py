37be014bb7c397a57bc9c909e69e62fc8d01c9af
eliberbell <liberbell@gmail.com>
2026-08-26 15:29:53 +0900

    covid_data2

--- lesson6/covid_data.py
+++ lesson6/covid_data.py
@@ -3,4 +3,6 @@ import geopandas as gpd
 import plotly.express as px
 import numpy as np
 
-df = pd.read_csv("data/owid-covid-latest.csv", encoding="utf-8")
+# df = pd.read_csv("data/owid-covid-latest.csv", encoding="utf-8")
+df = pd.read_csv("data/owid-covid-data-last-ecdc.csv", encoding="utf-8")
+print(df)
