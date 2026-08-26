00768935cd997d8263bfb3e48286a01b0540839b
eliberbell <liberbell@gmail.com>
2026-08-26 15:51:15 +0900

    covid_data4

--- lesson6/covid_data.py
+++ lesson6/covid_data.py
@@ -14,4 +14,7 @@ df_test = df[df["date"] == "2020-11-20"]
 print(df_test)
 
 avg_cases = df_test["new_cases"].median()
-px.choropleth(df_test, locations="iso_code", color="new_cases", range_color=[0, 10000], color_continuous_midpoint=avg_cases).show()
+# px.choropleth(df_test, locations="iso_code", color="new_cases", range_color=[0, 10000], color_continuous_midpoint=avg_cases).show()
+
+df_test["log_cases"] = df["new_cases"].apply(np.log10)
+px.choropleth(df_test, locations="iso_code", color="log_cases", range_color=[0, 10000], color_continuous_midpoint=avg_cases).show()
